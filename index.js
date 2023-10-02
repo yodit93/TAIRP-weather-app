6e1d896116bd7f1998ee15d8c8796b26d699a306
yodit93 <yoditabebe9391@gmail.com>
2023-10-02 20:22:55 +0300

    Revert "Used separate javscript files"
    This reverts commit 3c722b4827c5e3f50fddb9a64b50ce2f69db81ae.

--- index.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector('#city');
-const handleInput = (e) => {
-    const inputValue = e.target.value
-    localStorage.setItem('inputValue', JSON.stringify(inputValue));
-}
-
-input.addEventListener('change', (e) => {
-    handleInput(e);
-})
