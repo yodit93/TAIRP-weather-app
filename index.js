53433089f498b100a92c05fa8a0e53726ed00aff
yodit93 <yoditabebe9391@gmail.com>
2023-10-03 11:54:04 +0300

    Add validation and error handling

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const input = document.querySelector('#city');
+const getDataBtn = document.querySelector('.get-data');
+const error = document.querySelector('.error');
 const handleInput = (e) => {
     const inputValue = e.target.value
     localStorage.setItem('inputValue', JSON.stringify(inputValue));
-}
+};
 
-input.addEventListener('change', (e) => {
+input.addEventListener('input', (e) => {
+    error.textContent = "";
     handleInput(e);
+});
+
+getDataBtn.addEventListener('click', (e) => {
+    if(!input.value) {
+        e.preventDefault();
+        error.textContent = "Please enter a city name";
+    }
 })
+
